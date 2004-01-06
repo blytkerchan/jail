caa938bd8d9ae24afa09e489bf477d02c951ab62
blytkerchan <blytkerchan>
2004-01-06 08:46:26 +0000

    add files for polling the public

---
+++ jailust.questions.sql
@@ -0,0 +1,26 @@
+-- MySQL dump 9.09
+--
+-- Host: localhost    Database: jail
+-- ------------------------------------------------------
+-- Server version	4.0.16
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS questions;
+CREATE TABLE questions (
+  uid int(16) NOT NULL default '0',
+  question varchar(120) NOT NULL,
+  examples int(16) NOT NULL default '0',
+  answers int(16) NOT NULL default '0',
+  commentable int(1) NOT NULL default '0',
+  PRIMARY KEY  (uid)
+) TYPE=MyISAM;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO questions VALUES (0,'What\'s in a namespace?','0','0','1');
+
