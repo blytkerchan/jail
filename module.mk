ea680f50bbd1ea7069708c99bd7da48f85ccfbaa
blytkerchan <blytkerchan>
2004-08-02 20:47:22 +0000

    2004-08-02	blytkerchan 	* module.mk: add fetch_and_add.c 	[arch_CFLAGS]: new variable

--- module.mk
+++ module.mk
@@ -1,7 +1,10 @@
 arch_SRC = 	$(ARCH)/compare_and_exchange.c \
 		$(ARCH)/set.c \
 		$(ARCH)/increment.c \
-		$(ARCH)/decrement.c
+		$(ARCH)/decrement.c \
+		$(ARCH)/fetch_and_add.c
+
+arch_CFLAGS=-I$(srcdir)/arch/include
 
 SRC += $(patsubst %,arch/%,$(arch_SRC))
 
