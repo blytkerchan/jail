37953a7e6c44bb3c89c54da4a3a33550ed90cdef
blytkerchan <blytkerchan>
2004-08-17 23:50:30 +0000

    2004-08-17	blytkerchan	continue translation to C 	* rw_lock.c: %s/lt_rwlock_queue/lt_thread_queue/g 	%s/lt_rwlock_list/lt_thread_list/g

--- rw_lock.c
+++ rw_lock.c
@@ -39,19 +39,19 @@
 #define WRITER 2
 
 static void lt_rwlock_schedule(lt_rwlock_t * lock);
-static int lt_rwlock_list_remove(lt_thread_t * list, lt_thread_t * entry);
-static int lt_rwlock_queue_empty(lt_thread_t * queue);
-static int lt_rwlock_list_empty(lt_thread_t * list);
+static int lt_thread_list_remove(lt_thread_t * list, lt_thread_t * entry);
+static int lt_thread_queue_empty(lt_thread_t * queue);
+static int lt_thread_list_empty(lt_thread_t * list);
 static void lt_thread_wake(lt_thread_t * thread);
-static lt_thread_t * lt_rwlock_queue_first(lt_thread_t * queue);
+static lt_thread_t * lt_thread_queue_first(lt_thread_t * queue);
 static int atomic_swap_ptr(volatile void * ptr1, volatile void * ptr2);
-static lt_thread_t * lt_rwlock_queue_deq(lt_thread_t * queue);
-static void lt_rwlock_list_move(lt_thread_t * to, lt_thread_t * from);
-static void lt_rwlock_list_for_each(lt_thread_t * list, void (*)(lt_thread_t * thread));
-static void lt_rwlock_queue_enq(lt_thread_t * queue, lt_thread_t * thread);
+static lt_thread_t * lt_thread_queue_deq(lt_thread_t * queue);
+static void lt_thread_list_move(lt_thread_t * to, lt_thread_t * from);
+static void lt_thread_list_for_each(lt_thread_t * list, void (*)(lt_thread_t * thread));
+static void lt_thread_queue_enq(lt_thread_t * queue, lt_thread_t * thread);
 static void lt_thread_suspend(lt_thread_t * thread);
-static void lt_rwlock_list_insert(lt_thread_t * list, lt_thread_t * thread);
-static lt_thread_t * lt_rwlock_list_find(lt_thread_t * list, lt_rwlock_t * thread);
+static void lt_thread_list_insert(lt_thread_t * list, lt_thread_t * thread);
+static lt_thread_t * lt_thread_list_find(lt_thread_t * list, lt_rwlock_t * thread);
 
 void lt_rwlock_read_lock(lt_rwlock_t * lock)
 {
@@ -80,15 +80,15 @@ void lt_rwlock_write_lock(lt_rwlock_t * lock)
 void lt_rwlock_read_unlock(lt_rwlock_t * lock) 
 {
 	/* first, we get ourselves out of the readers list */
-	assert(lt_rwlock_list_remove(lock->readers[1], lt_thread_self()) == 0);
+	assert(lt_thread_list_remove(lock->readers[1], lt_thread_self()) == 0);
 	/* now, if the readers list is empty, there are no more active readers
 	 * and a writer is therefore eligable for the lock. Otherwise, we're
 	 * done */
-	if (lt_rwlock_queue_empty(lock->writers) != 0) 
+	if (lt_thread_queue_empty(lock->writers) != 0) 
 	{
-		if (lt_rwlock_list_empty(lock->readers[1]) == 0) 
+		if (lt_thread_list_empty(lock->readers[1]) == 0) 
 		{
-			lt_thread_wake(lt_rwlock_queue_first(lock->writers));
+			lt_thread_wake(lt_thread_queue_first(lock->writers));
 		}
 	}
 }
@@ -105,17 +105,17 @@ void lt_rwlock_write_unlock(lt_rwlock_t * lock)
 	 * list place_holder: empty list of threads;
 	 *
 	 * we assume readers[1] is empty */
-	assert(lt_rwlock_list_empty(lock->readers[1]));
+	assert(lt_thread_list_empty(lock->readers[1]));
 	/* so we put the place-holder at readers[0] */
 	atomic_swap_ptr(lock->readers[0], place_holder);
 	/* place_holder now contains readers[0]
 	 * we now de-queue ourselves */
-	assert(lt_thread_eq(lt_rwlock_queue_deq(lock->writers), lt_thread_self()) == 0);
+	assert(lt_thread_eq(lt_thread_queue_deq(lock->writers), lt_thread_self()) == 0);
 	/* if the writers queue is empty, it will schedule into readers[1];
 	 * if not, it will continue scheduling into readers[0], which is now the
 	 * place-holder. */
 	/* we add whatever we swapped out to readers[1] */
-	lt_rwlock_list_move(lock->readers[1], place_holder);
+	lt_thread_list_move(lock->readers[1], place_holder);
 	/* we swap again. As readers[0] only contains sleeping threads, we're sure
 	 * all the threads in there are asleep and won't think they're in
 	 * readers[1] */
@@ -125,9 +125,9 @@ void lt_rwlock_write_unlock(lt_rwlock_t * lock)
 	 * queue (which may have made it possible to schedule into readers[1]
 	 * to readers[1]. If the scheduler is still scheduling into readers[0],
 	 * these threads just got lucky */
-	lt_rwlock_list_move(lock->readers[1], place_holder);
+	lt_thread_list_move(lock->readers[1], place_holder);
 	/* now, we awake everyone in readers[1] */
-	lt_rwlock_list_for_each(lock->readers[1], lt_thread_wake);
+	lt_thread_list_for_each(lock->readers[1], lt_thread_wake);
 	/* and we're done */
 }
 
@@ -137,12 +137,12 @@ void lt_rwlock_schedule(lt_rwlock_t * lock)
 
 	/* now, the local thread is enqueued in the general queue. This guarantees
 	 * that at some time in the future, it will be scheduled */
-	lt_rwlock_queue_enq(lock->general, lt_thread_self());
+	lt_thread_queue_enq(lock->general, lt_thread_self());
 
 	/* now, if it's not the first one in the queue, it should do its own
 	 * scheduling. Otherwise, some other thread will take care of it and
 	 * we can suspend ourselves. */
-	if (lt_thread_eq(lt_rwlock_queue_first(lock->general), lt_thread_self()) != 0) 
+	if (lt_thread_eq(lt_thread_queue_first(lock->general), lt_thread_self()) != 0) 
 	{
 		lt_thread_suspend(lt_thread_self());
 	}
@@ -163,36 +163,36 @@ void lt_rwlock_schedule(lt_rwlock_t * lock)
 			 * adding to the two readers lists and the writers queue, and removing
 			 * from the general queue. */
 
-			while (!lt_rwlock_queue_empty(lock->general)) 
+			while (!lt_thread_queue_empty(lock->general)) 
 			{
 				/* get the next thread to schedule */
-				curr = lt_rwlock_queue_deq(lock->general);
+				curr = lt_thread_queue_deq(lock->general);
 				/* note that we don't handle ourselves specially here: when we're
 				 * done, we'll see where we are and act accordingly */
 				switch (curr->flag)
 				{
 				case READER :
-					if (lt_rwlock_queue_empty(lock->writers)) 
+					if (lt_thread_queue_empty(lock->writers)) 
 					{
 						/* if there are no writers, the thread (which is a reader) 
 						 * goes to the second readers list and can go on 
 						 * immediatly. */
-						lt_rwlock_list_insert(lock->readers[1], curr);
+						lt_thread_list_insert(lock->readers[1], curr);
 						lt_thread_wake(curr);
 					} 
 					else 
 					{
 						/* if there are writers, the thread will have to continue 
 						 * sleeping until the writers are done. */
-						lt_rwlock_list_insert(lock->readers[0], curr);
+						lt_thread_list_insert(lock->readers[0], curr);
 					}
 					break;
 				case WRITER :
 					/* in any case, we enqueue the writer */
-					lt_rwlock_queue_enq(lock->writers, curr);
+					lt_thread_queue_enq(lock->writers, curr);
 					/* we then look whether there are any active readers (in 
 					 * lock->readers[1]). If not, we wake the writer */
-					if (lt_rwlock_list_empty(lock->readers[1])) 
+					if (lt_thread_list_empty(lock->readers[1])) 
 						lt_thread_wake(curr);
 					break;
 				}
@@ -202,8 +202,8 @@ void lt_rwlock_schedule(lt_rwlock_t * lock)
 			assert(compare_and_exchange_ptr(&exp, &(lock->scheduler), NULL) == 0);
 			/* if there are any threads in the general queue now, we wake up the
 			 * first and let him handle the scheduling */
-			if (!lt_rwlock_queue_empty(lock->general))
-				lt_thread_wake(lt_rwlock_queue_first(lock->general));
+			if (!lt_thread_queue_empty(lock->general))
+				lt_thread_wake(lt_thread_queue_first(lock->general));
 		}
 	}	
 
@@ -215,14 +215,14 @@ void lt_rwlock_schedule(lt_rwlock_t * lock)
 		{
 		case READER :
 			/* readers can continue if they're in the second list */
-			if (lt_rwlock_list_find(lock->readers[1], lt_thread_self()) == NULL)
+			if (lt_thread_list_find(lock->readers[1], lt_thread_self()) == NULL)
 				return; /* break out of the endless loop */
 			break;
 		case WRITER :
 			/* writers can continue if they're the first one in the queue
 			 * and the second readers list is empty */
-			if (lt_rwlock_list_empty(lock->readers[1]) && 
-				(lt_thread_eq(lt_thread_self(), lt_rwlock_queue_first(lock->writers)) == 0))
+			if (lt_thread_list_empty(lock->readers[1]) && 
+				(lt_thread_eq(lt_thread_self(), lt_thread_queue_first(lock->writers)) == 0))
 				return; /* break out of the endless loop */
 			break;
 		}
