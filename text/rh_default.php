8fb36d25c5d70568f00bf77f1ef7a376ded3039d
blytkerchan <blytkerchan>
2004-08-18 17:04:31 +0000

    2004-08-18	blytkerchan 	* rh_default.php: randomly pick the png between two candidates

--- text/rh_default.php
+++ text/rh_default.php
@@ -1,1 +1,1 @@
-<img src="banners/banner1.png" alt="Jail: Using Silly Terms - Because I Like It!"/>
+<img width="180" height="360" src="banners/banner<?php echo rand(1,2); ?>.png" alt="Jail: Using Silly Terms - Because I Like It!"/>
