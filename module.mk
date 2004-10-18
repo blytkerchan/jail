b0bf34df02cab0c50cd64d8aa24480ec8ffa7f80
blytkerchan <blytkerchan>
2004-10-18 16:57:26 +0000

    2004-10-18	blytkerchan	remove references to C++ hash implementation 	* module.mk: remove anything related to the C++ hash implementation

--- module.mk
+++ module.mk
@@ -1,10 +1,4 @@
 # the library
-libcontain_CXX_SRC += 	Hash.cc \
-			StringHash.cc \
-			IntHash.cc \
-			NocaseStringHash.cc \
-			IniHash.cc \
-			c_interface.cc
 libcontain_SRC +=	stack.c \
 			queue.c \
 			hash.c \
@@ -18,6 +12,5 @@ libcontain_GLIB_SRC = 	glib/ghash.c \
 			glib/gprimes.c
 
 SRC += $(patsubst %,libcontain/%,$(libcontain_SRC))
-CXX_SRC += $(patsubst %,libcontain/%,$(libcontain_SRC))
 GLIB_SRC += $(patsubst %,libcontain/%,$(libcontain_SRC))
 
