9e884203ebc234f375891be649c20bf73eb63534
blytkerchan <blytkerchan>
2004-10-20 13:54:13 +0000

    2004-10-20	blytkerchan 	* module.mk [arch_SRC]: add membar.c

--- module.mk
+++ module.mk
@@ -2,7 +2,8 @@ arch_SRC = 	$(ARCH)/compare_and_exchange.c \
 		$(ARCH)/set.c \
 		$(ARCH)/increment.c \
 		$(ARCH)/decrement.c \
-		$(ARCH)/fetch_and_add.c
+		$(ARCH)/fetch_and_add.c \
+		$(ARCH)/membar.c
 
 SRC += $(patsubst %,arch/%,$(arch_SRC))
 
