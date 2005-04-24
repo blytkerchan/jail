7633572f0cf5598a23a5c86965283d04567c16f2
blytkerchan <blytkerchan>
2005-04-24 00:54:41 +0000

    2005-04-23	blytkerchan 	* module.ml: add support for INSTALL_HEADERS

--- module.mk
+++ module.mk
@@ -5,3 +5,6 @@ libconf_SRC += 	libconf.c \
 		posix/readconf.cc
 
 SRC += $(patsubst %,libconf/%,$(libconf_SRC))
+libconf_INSTALL_HEADERS += $(patsubst %.c,%.h,$(libconf_SRC))
+INSTALL_HEADERS += $(patsubst %,libcontain/%,$(libconf_INSTALL_HEADERS))
+
