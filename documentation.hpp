d20478d5329f50ba95ca17073fdfb13be74aa30a
blytkerchan <blytkerchan>
2006-08-14 01:50:58 +0000

    2006-08-13	blytkerchan 	* SubjectTraits.h: new file 	* TraversalPolicy.h: ditto 	* Visitor.h: ditto 	* VisitorTraits.h: ditto 	* documentation.hpp: ditto

---
+++ documentation.hpp
@@ -0,0 +1,10 @@
+/** \mainpage An implementation of the Visitor pattern
+ * I recently read an article in IEEE Computer about the Visitor pattern and
+ * "Componentization" ("Componentization: The Visitor Example", B. Meyer & 
+ * K. Arnout, IEEE Computer, July 2006 (Vol. 39, No. 7) pp. 23-30). I found
+ * their argument, that design patterns make for reusable patterns but not
+ * reusable software components, compelling and the example of the Visitor
+ * pattern very interesting, as a Visitor is definitely something you'd need
+ * to visit a parse tree written in C++.
+ * I checked Andrei Alexandrescu's solution from Modern C++ Design
+ * (Addison-Wesley, 2001) and found it oddly lacked flexibility... */
